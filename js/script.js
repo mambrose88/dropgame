7b9e09bd5d010b1345f46ceb0659b6c96fefc20e
mambrose88 <m.ambrose88@gmail.com>
2013-11-18 09:40:08 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -4,12 +4,36 @@ var drop_array = new Array();
 //vars to name the timers that make drops spawn and move every so often
 var spawntimer;
 var movetimer;
+//create a bucket obj, instance of the bucket class below not on stage yet
+var user_bucket= new Bucket(25,200);
 function init(){
+	//put that bucket on stage
+	user_bucket.create();
+	//when a key is pressed, do a function that responds depending on which key
+	document.onkeydown = function(e){ checkKey(e);};
 	//set an interval to do the spawn function every 2 seconds
 	spawntimer = setInterval(spawn,2000);
 	//set an interval to move the drops 20 times a second
 	movetimer = setInterval(movealldrops,1000/20);
 }
+
+function checkKey(e){
+	//set up the event again for certain browsers
+	e = e || window.event;
+	//right arrow key
+	if(e.keyCode == '39'){
+		//add to buckets x will move it a little to right
+	user_bucket.x+=15;
+		user_bucket.item_on_page.style.left = user_bucket.x + "px";
+	}
+	
+	if(e.keyCode == '37'){
+		//add to buckets x will move it a little to right
+	user_bucket.x-=15;
+		user_bucket.item_on_page.style.left = user_bucket.x + "px";
+	}
+}//close checkKey
+
 function spawn() {
 	//make an object based on the Drop Class:
 	var anotherdrop = new Drop();
@@ -38,6 +62,8 @@ for (var i=0; i<drop_array.length; i++){
 function Drop(){
 	this.x; //starts empty, will keep track of each Drop's left-right position
 	this.y; //starts empty, will keep track of each Drop's up-down position
+	this.width=50;
+	this.height=50;
 	this.item_on_page;
 	/** function does lots of things when a Drop gets created on the page
 	*
@@ -73,5 +99,31 @@ function Drop(){
 	//splice it out of the array
 	drop_array.splice(this_drops_index_number,1);
 		
+	}
+} //close the drop Class
+
+//let's make a Class (blueprint) for each Drop we generate
+function Bucket(x,y){
+	this.x=x; //starts empty, will keep track of each Drop's left-right position
+	this.y=y; //starts empty, will keep track of each Drop's up-down position
+	this.width=100;
+	this.height=70;
+	this.item_on_page;
+	/** function does lots of things when a bucket gets created on the page
+	*
+	*/
+	this.create = function(){
+		//make a div element in the HTML, store it into the item-on-page we set up above.
+		this.item_on_page = document.createElement("div");
+		//use those x and y coordinates in the CSS to position the bucket:
+		this.item_on_page.style.left = this.x + "px";
+		this.item_on_page.style.top = this.y + "px";
+		//attach the item to our HTML hierarchy, as a child of the body:
+		document.getElementsByTagName("body")[0].appendChild(this.item_on_page);
+	}
+	/** function does lots of things when a bucket is removed from the page
+	*
+	*/
+	this.destroy = function(){
 	}
 } //close the Class
